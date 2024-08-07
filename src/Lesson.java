36f6623ecdd428c7371480207e3a21fa796a0b67
noerror606 <fizryk.31@gmail.com>
2024-08-07 19:30:36 +0300

    refactor code

--- src/Lesson.java
+++ src/Lesson.java
@@ -1,9 +1,10 @@
 public class Lesson {
-
+    //1
     public void printThreeWords() {
         System.out.println("Orange\nBanana\nApple");
     }
 
+    //2
     public void checkSumSign() {
         int a = 10;
         int b = -15;
@@ -14,8 +15,10 @@ public class Lesson {
         }
     }
 
+    //3
     public void printColor() {
         int value = 255;
+
         if (value <= 0) {
             System.out.println("Красный");
         } else if (value <= 100) {
@@ -25,43 +28,40 @@ public class Lesson {
         }
     }
 
+    //4
     public void compareNumbers() {
-        int a = 10;
-        int b = 30;
+        int a = 10, b = 30;
         if (a >= b) {
             System.out.println("a >= b");
         } else {
             System.out.println("a < b");
         }
-
     }
 
+    //5
     public boolean sumChecker(int a, int b) {
         System.out.println(a + b >= 10 && a + b <= 20);
         return a + b >= 10 && a + b <= 20;
     }
 
+    //6
     public void rationalNumbers(int a) {
         if (a >= 0) {
             System.out.println("Число положительное");
+
         } else {
             System.out.println("Число отрицательное");
         }
-
     }
 
+    //7
     public boolean rationalNumbersBoolean(int a) {
-        boolean result;
-        if (a >= 0) {
-            result = true;
-        } else {
-            result = false;
-        }
-
+        boolean result = a >= 0 ? true : false;
         System.out.println(result);
         return result;
     }
 
+    //8
     public void stringNum(int num, String string) {
         for (int i = 0; i < num; ++i) {
             System.out.println(string);
@@ -69,61 +69,52 @@ public class Lesson {
 
     }
 
+    //9
     public boolean leapYear(int year) {
-        if (year % 4 == 0 && year % 400 == 0) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
             return true;
         } else {
-            return year % 100 == 0 ? false : false;
+            return false;
         }
     }
 
+    //10
     public void swap() {
         int[] array = new int[]{1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0};
         System.out.print("Массив до изменения: ");
-
-        int i;
-        for (i = 0; i < array.length; ++i) {
+        for (int i = 0; i < array.length; ++i) {
             System.out.print(array[i] + " ");
         }
-
-        for (i = 0; i < array.length; ++i) {
+        for (int i = 0; i < array.length; ++i) {
             if (array[i] == 1) {
                 array[i] = 0;
             } else {
                 array[i] = 1;
             }
         }
-
         System.out.print("Массив после изменения: ");
-
-        for (i = 0; i < array.length; ++i) {
+        for (int i = 0; i < array.length; ++i) {
             System.out.print(array[i] + " ");
         }
-
-        System.out.println("");
     }
 
+    //11
     public void arrayFill() {
         int[] newArray = new int[100];
-
         for (int i = 0; i < newArray.length; ++i) {
             System.out.print((newArray[i] = i + 1) + " ");
         }
-
-        System.out.println("");
     }
 
+    //12
     public void numberMultiplication() {
         int[] array = new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         System.out.print("Массив до изменения: ");
-
         int i;
         for (i = 0; i < array.length; ++i) {
             System.out.print(array[i] + " ");
         }
-
         System.out.print("Массив после изменения: ");
-
         for (i = 0; i < array.length; ++i) {
             if (array[i] < 6) {
                 array[i] *= 2;
@@ -131,38 +122,32 @@ public class Lesson {
 
             System.out.print(array[i] + " ");
         }
-
-        System.out.println(" ");
     }
 
+    //13
     public void matrix() {
         int m = 5;
         int[][] matrix = new int[m][m];
-
         int i;
         for (i = 0; i < m; ++i) {
             matrix[i][i] = 1;
             matrix[i][m - i - 1] = 1;
         }
-
         for (i = 0; i < m; ++i) {
             matrix[i][m - i - 1] = 1;
         }
-
         for (i = 0; i < m; ++i) {
             for (int j = 0; j < m; ++j) {
                 System.out.print(matrix[i][j] + " ");
             }
-
             System.out.println();
         }
-
     }
 
-    public static String[] initialValue(int len, String initialValue) {
-        String[] array = new String[len];
+    //14
+    public static int[] initialValue(int len, int initialValue) {
+        int[] array = new int[len];
         System.out.print("Массив:");
-
         for (int i = 0; i < len; ++i) {
             array[i] = initialValue;
             System.out.print(" " + array[i]);
--- src/Main.java
+++ src/Main.java
@@ -4,41 +4,58 @@ public class Main {
     public static void main(String[] args) {
         Lesson lesson = new Lesson();
         Scanner sc = new Scanner(System.in);
+
+        //1
         lesson.printThreeWords();
+        //2
         lesson.checkSumSign();
+        //3
         lesson.printColor();
+        //4
         lesson.compareNumbers();
+        //5
         System.out.print("Введите число а: ");
         int sumA = sc.nextInt();
         System.out.print("Введите число b: ");
         int sumB = sc.nextInt();
         lesson.sumChecker(sumA, sumB);
+        //6
         System.out.print("Введите число: ");
         int Rational = sc.nextInt();
         lesson.rationalNumbers(Rational);
+        //7
         System.out.print("Введите число: ");
         int RationalBool = sc.nextInt();
         lesson.rationalNumbersBoolean(RationalBool);
+        //8
         System.out.print("Введите число: ");
         int num = sc.nextInt();
         sc.nextLine();
         System.out.print("Введите строку: ");
         String string = sc.nextLine();
         lesson.stringNum(num, string);
+        //9
         System.out.print("Введите год: ");
         int year = sc.nextInt();
         lesson.leapYear(year);
+        //10
         boolean result = lesson.leapYear(year);
         System.out.println("Год " + year + " високосный: " + result);
         lesson.swap();
+        System.out.println(" ");
+        //11
         lesson.arrayFill();
+        System.out.println(" ");
+        //12
         lesson.numberMultiplication();
+        System.out.println(" ");
+        //13
         lesson.matrix();
-        System.out.print("Введите число: ");
+        //14
+        System.out.print("Введите количество: ");
         int len = sc.nextInt();
-        System.out.print("Введите строку: ");
-        sc.nextLine();
-        String initialValue = sc.nextLine();
+        System.out.print("Введите число: ");
+        int initialValue = sc.nextInt();
         lesson.initialValue(len, initialValue);
     }
 }
